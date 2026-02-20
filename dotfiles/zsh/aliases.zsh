b371967a9af91632f026087c2f8eb823bd69f54a
Thomas Leroy <5416321+thomas-leroy@users.noreply.github.com>
2026-02-20 14:21:35 +0100

    feat: add missing alliasses

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -10,3 +10,13 @@ if command -v fd >/dev/null 2>&1; then
 elif command -v fdfind >/dev/null 2>&1; then
   alias find='fdfind'
 fi
+
+alias ..="cd .."
+alias ls='ls -G'
+
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+alias grep="grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}"
